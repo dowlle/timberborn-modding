ce64e0d217b2a03be4ca12c107c1a833e8a8b277
Stef <stefappelhof@gmail.com>
2026-03-16 15:44:23 +0100

    Add missing System.Reflection using for BindingFlags

--- Assets/Mods/Archipelago/ArchipelagoSaveData.cs
+++ Assets/Mods/Archipelago/ArchipelagoSaveData.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Timberborn.GameFactionSystem;
 using Timberborn.Persistence;
 using Timberborn.SingletonSystem;
